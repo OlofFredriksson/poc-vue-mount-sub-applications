b1448039aaede2d81740895b88c4b4e7659081b9
Olof Fredriksson <oloflolof@gmail.com>
2025-11-10 09:26:28 +0100

    fix: uppgift1 as lib mode

--- packages/uppgift1/vite.config.js
+++ packages/uppgift1/vite.config.js
@@ -1,8 +1,11 @@
+import { resolve } from "node:path";
 import { URL, fileURLToPath } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +13,14 @@ export default defineConfig({
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    define: { "process.env": '"production"' },
+    build: {
+        lib: {
+            formats: ["es"],
+            entry: resolve(__dirname, "src/index.ts"),
+            name: "uppgift1",
         },
     },
 });
